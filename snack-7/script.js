9a51ffc252a7413a31877eeb81202b5d7bcb4a9f
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-02-21 21:06:41 +0100

    finish

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,51 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+//col for 
+let student;
+
+for (let i = 0; i < students.length; i++) {
+  if (students[i].id === 2) {
+    student = students[i];
+    break; // si ferma appena trova lo studente
+  }
+}
+
+console.log(student);
+
+// uso .find  restituisce il primo elemento dell’array che soddisfa la condizione => in questo caso è il metodo più corretto
+const student1 = students.find(studente => studente.id === 2);
+console.log(student1);
+
+//con .filter
+const array = students.filter(studente => studente.id === 2);
+console.log(array); //La proprietà length dell’array è 1, perché c’è un elemento dentro
+
+//.find e .filter sembrano uguali perchè in questo caso stampano la stessa cosa ma in realtà
+//si comportano in maniera differente :
+//find => restituisce UN SINGOLO OGGETTO (il primo che soddisfa la condizione)
+//filter => restituisce UN ARRAY di oggetti che soddisfano la condizione (anche se contiene solo 1 elemento)
+
+//------------------------------------
+//MIE PROVE
+//x estrarre solo l'id (mi stampa 2)
+
+//con .find(...).id restituisce l’oggetto completo ed accede alla proprietà id
+const studentId = students.find(studente => studente.id === 2).id;
+console.log(studentId); 
+// PRO               //CONTRO
+//Diretto             Non puoi estrarre più proprietà in una riga
+//semplice 
+
+
+//DESTRUTTURAZIONE
+//oppure const { id } = find(...) estrae solo la proprietà id e crea una variabile chiamata id
+const { id } = students.find(s => s.id === 2); // (s => s.id === 2) è la mia condizione
+console.log(id); 
+//con questo metodo posso estrarre anche più proprietà
+const { name, age } = students.find(s => s.id === 2);
+console.log(name, age);
+//PRO                               //CONTRO
+//Pulito                              più “verboso” per una sola proprietà
+// puoi estrarre più proprietà
--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,30 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+//col for
+
+for (let i=0; i<students.length; i++){
+  if(students[i].name=='Marco Lanci'){
+    console.log(students[i].class);
+    break;
+  }
+}
+
+// o 
+ let classeStudente ='';
+  for (let i=0; i<students.length; i++){
+    if (students[i].name=='Marco Lanci'){
+      classeStudente= students[i].class;
+      break;
+    }
+  }
+  console.log(classeStudente);
+
+  //con .find
+  const student1 = students.find(studente=>studente.name==='Marco Lanci').class;
+  console.log(student1);
+
+  //DESTRUTTURAZIONE
+  const {class : classeStudente1 } = students.find(studente=>studente.name==='Marco Lanci'); //class è una parola riservata in JavaScript, quindi non posso usarla direttamente come nome di variabile nella destrutturazione, la rinomino usando i due punti :
+  console.log (classeStudente1); 
